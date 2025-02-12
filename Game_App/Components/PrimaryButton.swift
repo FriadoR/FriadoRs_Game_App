2c5e41e94e1fdd6c608041b8d2e42ac2adc8a4af
Sachenko Sergey <braun.krieg@googlemail.com>
2025-02-12 18:22:59 +0300

    create folder and custom button

---
+++ Game_App/Components/PrimaryButton.swift
@@ -0,0 +1,27 @@
+//
+//  PrimaryButton.swift
+//  Game_App
+//
+//  Created by Сергей Саченко on 12.02.2025.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    var text: String
+    var background: Color = Color("AccentColor")
+    
+    var body: some View {
+        Text("Next")
+            .foregroundColor(.white)
+            .padding()
+            .padding(.horizontal)
+            .background(background)
+            .cornerRadius(30)
+            .shadow(radius: 10)
+    }
+}
+
+#Preview {
+    PrimaryButton(text: "Next")
+}
