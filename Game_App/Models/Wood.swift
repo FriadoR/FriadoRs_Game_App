8261041a9e2293a64ab0f64fa4d76ab7600cbe27
Sachenko Sergey <braun.krieg@googlemail.com>
2025-02-14 14:33:24 +0300

    lost commit ;)

--- Game_App/Models/Wood.swift
+++ Game_App/Models/Wood.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+struct Wood: Decodable {
+    var results: [Result]
+    
+    struct Result: Decodable, Identifiable {
+        
+        var id: UUID {
+            UUID()
+        }
+        
+        var category: String
+        var type: String
+        var difficulty: String
+        var question: String
+        var correctAnswer: String
+        var incorrectAnswers: [String]
+        
+        var formatedQuestion: AttributedString {
+            do {
+                return try AttributedString(markdown: question)
+            } catch {
+                print("Error setting formatedQuestion: \(error)")
+                return ""
+            }
+        }
+    }
+}
