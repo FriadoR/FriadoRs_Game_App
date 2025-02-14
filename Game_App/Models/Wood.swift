36ff4b927c5efb3c993990a0648ec685ae3223ec
Sachenko Sergey <braun.krieg@googlemail.com>
2025-02-14 14:32:42 +0300

    create new important model have a new variables and the main struct Wood

---
+++ Game_App/Models/Wood.swift
@@ -0,0 +1,8 @@
+//
+//  Wood.swift
+//  Wood Game
+//
+//  Created by Сергей Саченко on 14.02.2025.
+//
+
+import Foundation
--- Game_App/WoodGame.swift
+++ Game_App/WoodGame.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct Game_AppApp: App {
+struct WoodGame: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
