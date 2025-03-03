5324c2acec647e8cbd3829ae49e0590483ebc851
Sachenko Sergey <braun.krieg@googlemail.com>
2025-03-03 11:52:10 +0300

    #2 editing details

--- Game_App/Views/LoginView.swift
+++ Game_App/Views/LoginView.swift
@@ -7,10 +7,9 @@
 
 import SwiftUI
 import FirebaseAuth
-import DotLottie
 
 struct LoginView: View {
-    @State private var activateTab: Tab = .login
+    @State private var activeTab: Tab = .login
     @State private var emailAddress: String = ""
     @State private var password: String = ""
     @State private var reEnterPassword: String = ""
@@ -28,38 +27,73 @@ struct LoginView: View {
                         .keyboardType(.emailAddress)
                         .customTextFieldStyle("person")
                     
+                    SecureField("Password", text: $password)
+                        .customTextFieldStyle("person", 0, activeTab == .login ? 10 : 0)
+                    
+                    if activeTab == .signUp {
+                        SecureField("Re-enter Password", text: $reEnterPassword)
+                            .customTextFieldStyle("person", 0, activeTab != .login ? 10 : 0)
+                    }
+                    
                 } header: {
-                    Picker("", selection: $activateTab) {
+                    Picker("", selection: $activeTab) {
                         ForEach(Tab.allCases, id: \.rawValue) {
                             Text($0.rawValue)
                                 .tag($0)
                         }
                     }
                     .pickerStyle(.segmented)
-                    .listRowInsets(.init(top: 15, leading: 0, bottom: 0, trailing: 15))
+                    .listRowInsets(.init(top: 15, leading: 0, bottom: 15, trailing: 0))
                     .listRowSeparator(.hidden)
                     
                 } footer: {
+                    VStack(alignment: .trailing, spacing: 12, content: {
+                        if activeTab == .login {
+                            Button("Forgot Password?") {
+                                
+                            }
+                            .font(.caption)
+                            .tint(Color("AccentWoodColor"))
+                            
+                        }
+                        Button(action: {}, label: {
+                            HStack(spacing: 12) {
+                                Text(activeTab == .login ? "Login" : "Create Account")
+                                
+                                Image(systemName: "arrow.right")
+                                    .font(.callout)
+                            }
+                            .padding(.horizontal, 10)
+                        })
+                        .buttonStyle(.borderedProminent)
+                        .buttonBorderShape(.capsule)
+                    })
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .listRowInsets(.init(top: 15, leading: 0, bottom: 0, trailing: 0))
+                    }
                     
                 }
             }
+            .animation(.snappy, value: activeTab)
+            .listStyle(.insetGrouped)
+            .navigationBarTitle("Welcome")
             
             
-            NavigationLink("", destination: ContentView())
-                .navigationDestination(isPresented: $navigateToContentView) {
-                    ContentView()
-                }
-                .hidden()
-                .navigationTitle("Welcome")
-                .navigationBarBackButtonHidden(true)
-            
+//            NavigationLink("", destination: ContentView())
+//                .navigationDestination(isPresented: $navigateToContentView) {
+//                    ContentView()
+//                }
+//                .hidden()
+//                .navigationTitle("Welcome")
+//                .navigationBarBackButtonHidden(true)
+//            
             
             
         }
-        .padding(.bottom, 50)
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .ignoresSafeArea(.all)
-        //                .background(background)
+//        .padding(.bottom, 50)
+//        .frame(maxWidth: .infinity, maxHeight: .infinity)
+//        .ignoresSafeArea(.all)
+//        .background(background)
         
         
     }
@@ -68,11 +102,11 @@ struct LoginView: View {
         case signUp = "Sing Up"
         
     }
-}
+
 
 fileprivate extension View {
     @ViewBuilder
-    func customTextFieldStyle(_ icon: String? = nil, paddingTop: CGFloat = 0, _ paddingBottom: CGFloat = 0) -> some View {
+    func customTextFieldStyle(_ icon: String? = nil, _ paddingTop: CGFloat = 0, _ paddingBottom: CGFloat = 0) -> some View {
         HStack(spacing: 12) {
             if let icon {
                 Image(systemName: icon)
