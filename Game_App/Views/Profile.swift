27258908c2466999c23ffac922a37735f21086dc
Sachenko Sergey <braun.krieg@googlemail.com>
2025-03-21 15:30:50 +0300

    create profile view

---
+++ Game_App/Views/Profile.swift
@@ -0,0 +1,18 @@
+//
+//  Profile.swift
+//  Wood Game
+//
+//  Created by Сергей Саченко on 21.03.2025.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Profile()
+}
